86535acbeb985b8f2f3597ed26f58517c809dbaf
AlphaX <normsp@gmail.com>
2025-04-12 23:35:06 -0400

    Update rusefi_wideband.cpp
    im so sick

--- firmware/controllers/can/rusefi_wideband.cpp
+++ firmware/controllers/can/rusefi_wideband.cpp
@@ -10,12 +10,11 @@
 #include "rusefi_wideband.h"
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
-#include "wideband_firmware/for_rusefi/wideband_can.h"
+#include "wideband_can.h"
 #pragma GCC diagnostic pop
 
 // This file contains an array called build_wideband_noboot_bin
 // This array contains the firmware image for the wideband contoller
-#include "wideband_firmware/for_rusefi/wideband_image.h"
 
 #define EVT_BOOTLOADER_ACK EVENT_MASK(0)
 
@@ -37,76 +36,6 @@ static size_t getWidebandBus() {
 }
 
 void updateWidebandFirmware() {
-	size_t bus = getWidebandBus();
-
-	// Clear any pending acks for this thread
-	chEvtGetAndClearEvents(EVT_BOOTLOADER_ACK);
-
-	// Send messages to the current thread when acks come in
-	waitingBootloaderThread = chThdGetSelfX();
-
-	efiPrintf("***************************************");
-	efiPrintf("        WIDEBAND FIRMWARE UPDATE");
-	efiPrintf("***************************************");
-	efiPrintf("Wideband Update: Rebooting to bootloader...");
-
-	// The first request will reboot the chip (if necessary), and the second one will enable bootloader mode
-	// If the chip was already in bootloader (aka manual mode), then that's ok - the second request will
-	// just be safely ignored (but acked)
-	for (int i = 0; i < 2; i++) {
-		{
-			// Send bootloader entry command
-			CanTxMessage m(CanCategory::WBO_SERVICE, WB_BL_ENTER, 0, bus, true);
-		}
-
-		if (!waitAck()) {
-			efiPrintf("Wideband Update ERROR: Expected ACK from entry to bootloader, didn't get one.");
-			return;
-		}
-
-		// Let the controller reboot (and show blinky lights for a second before the update begins)
-		chThdSleepMilliseconds(200);
-	}
-
-	efiPrintf("Wideband Update: in update mode, erasing flash...");
-
-	{
-		// Erase flash - opcode 1, magic value 0x5A5A
-		CanTxMessage m(CanCategory::WBO_SERVICE, 0xEF1'5A5A, 0, bus, true);
-	}
-
-	if (!waitAck()) {
-		efiPrintf("Wideband Update ERROR: Expected ACK from flash erase command, didn't get one.");
-		return;
-	}
-
-	size_t totalSize = sizeof(build_wideband_image_bin);
-
-	efiPrintf("Wideband Update: Flash erased! Sending %d bytes...", totalSize);
-
-	// Send flash data 8 bytes at a time
-	for (size_t i = 0; i < totalSize; i += 8) {
-		{
-			CanTxMessage m(CanCategory::WBO_SERVICE, 0xEF2'0000 + i, 8, bus, true);
-			memcpy(&m[0], build_wideband_image_bin + i, 8);
-		}
-
-		if (!waitAck()) {
-			efiPrintf("Wideband Update ERROR: Expected ACK from data write, didn't get one.");
-			return;
-		}
-	}
-
-	efiPrintf("Wideband Update: Update complete! Rebooting controller.");
-
-	{
-		// Reboot to firmware!
-		CanTxMessage m(CanCategory::WBO_SERVICE, 0xEF3'0000, 0, bus, true);
-	}
-
-	waitAck();
-
-	waitingBootloaderThread = nullptr;
 }
 
 void setWidebandOffset(uint8_t index) {
