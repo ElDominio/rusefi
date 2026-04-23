2847aa69fd4861e356f433090910e81e7ced3972
AlphaX <normsp@gmail.com>
2026-04-22 21:06:54 -0400

    s550 build test

---
+++ firmware/config/boards/hellen/alphax-s550-pnp/board.mk
@@ -0,0 +1,36 @@
+# Combine the related files for a specific platform and MCU.
+
+# Target ECU board design
+BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
+
+
+
+# SENT
+DDEFS += -DSTM32_ICU_USE_TIM1=TRUE -DSTM32_PWM_USE_TIM1=FALSE
+DDEFS += -DEFI_SENT_SUPPORT=TRUE
+
+# TLS115_PG
+DDEFS += -DDIAG_5VP_PIN=Gpio::MM176_OUT_PWM10
+
+LED_CRITICAL_ERROR_BRAIN_PIN = -DLED_CRITICAL_ERROR_BRAIN_PIN=H176_MCU_MEGA_LED1_RED
+include $(BOARDS_DIR)/hellen/hellen-common-mega176.mk
+
+ifeq ($(PROJECT_CPU),ARCH_STM32F7)
+	DDEFS += -DCH_DBG_ENABLE_ASSERTS=FALSE
+	DDEFS += -DENABLE_PERF_TRACE=FALSE
+else ifeq ($(PROJECT_CPU),ARCH_STM32F4)
+    # This board has trigger scope hardware!
+    DDEFS += -DTRIGGER_SCOPE
+    # serial ports only on F4
+	DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
+else
+$(error Unsupported PROJECT_CPU [$(PROJECT_CPU)])
+endif
+
+
+DDEFS += -DLUA_STM32_STANDBY=1
+
+
+
+# watchdog suddenly i see it #8699
+DDEFS += -DHAL_USE_WDG=FALSE
---
+++ firmware/config/boards/hellen/alphax-s550-pnp/compile_alphax-s550-bundle.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd ../../../.. && bash bin/compile.sh config/boards/hellen/alphax-s550-pnp/meta-info-alphax-s550.env bundle
